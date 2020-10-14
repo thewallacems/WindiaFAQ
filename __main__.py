b4172359fb9923a908b7e998dfacc8cadd0d2cf2
Wallace <thewallacems@gmail.com>
2020-10-14 14:34:21 -0400

    add systemctl service

--- __main__.py
+++ __main__.py
@@ -7,15 +7,19 @@ from bot import WindiaFAQ
 from intentsloader import IntentsCSVLoader
 from configloader import ConfigINILoader
 
-config = ConfigINILoader('windia.ini').load()
+DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_PATH = os.path.join(DIR, 'windia.ini')
+INTENTS_PATH = os.path.join(DIR, 'intents.csv')
+COGS_PATH = os.path.join(DIR, 'cogs')
+
+config = ConfigINILoader(CONFIG_PATH).load()
 
 token = config['Bot']['Token']
 prefix = config['Bot']['Prefix']
 
-intents = IntentsCSVLoader('intents.csv').load()
+intents = IntentsCSVLoader(INTENTS_PATH).load()
 bot = WindiaFAQ(prefix, config, intents)
 
-COGS_PATH = './cogs/'
 cogs = [f'cogs.{file[:-3]}' for file in os.listdir(COGS_PATH) if file.endswith('.py')]
 
 for cog in cogs:
